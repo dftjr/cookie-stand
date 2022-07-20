b42558136e21d19d4565fb3939a1d8d0d62df67f
David Tusia <105877296+dftjr@users.noreply.github.com>
2022-07-20 01:11:44 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,15 +10,8 @@ let tableFooter = document.querySelector('tfoot');
 console.log(tableFooter);
 
 
-// GlobVar1: Time of days
 let timeOfDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-// console.log(timeOfDay);
-// all tests pass
-// end of GlobVar1
 
-
-// Constructor: Creates a new store from data saved in Section1
-// Section1: Data placeholders
 function Store(name, min, max, avg, sales, totals) {
   this.name = name;
   this.min = min;
@@ -26,20 +19,11 @@ function Store(name, min, max, avg, sales, totals) {
   this.avg = avg;
   this.sales = [];
   this.totals = 0;
-  // all tests pass
-  // end of Section1
 
-
-  // Func1: Generates random number
   function getRandomCustPerHour() {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
-  // console.log(getRandomCustPerHour());
-  // all tests pass
-  // end of Func1
-
-
-  // Func2: Generates a number from Global1 * Global2
+  
   function cookiesPerHour() {
     for (let i = 0; i < timeOfDay.length; i++) {
     let customersThisHour = getRandomCustPerHour();
@@ -52,35 +36,39 @@ function Store(name, min, max, avg, sales, totals) {
   } 
   cookiesPerHour();
   console.log(sales);
-
+  
   this.renderTable = function() {
-    let trData = document.createElement('tr');
-    tableBody.appendChild(trData);
+    let trRow = document.createElement('tr');
+    tableBody.appendChild(trRow);
+
+    let tdName = document.createElement('td');
+    trRow.appendChild(tdName);
+    tdName.textContent = name;
 
     for (let i = 0; i < sales.length; i++) {
       let tdCookies = document.createElement('td');
-      tableBody.appendChild(tdCookies);
+      trRow.appendChild(tdCookies);
       tdCookies.textContent = sales[i];
     }
+
+    let tdTotal = document.createElement('td');
+    trRow.appendChild(tdTotal);
+    tdTotal.textContent = totals;
   } 
   this.renderTable();
-  // console.log(totals);
 }
 
-
-this.renderTableFooter = function() {
-  cookiesPerHour();
-  for (let i = 0; i < timeOfDay.length; i++) {
-  let tfData = document.createElement('tr');
-  tableFooter.appendChild(tfData);
-  tfData.textContent = totals;
+function renderTableFooter() {
+  for (let i = -1; i < timeOfDay.length; i++) {
+    let thData = document.createElement('td');
+    tableFooter.appendChild(thData);
+    thData.textContent = timeOfDay[i];
   }
 }
-
-
+renderTableFooter();
 
 function renderTableHeader() {
-  for (let i = 0; i < timeOfDay.length; i++) {
+  for (let i = -1; i < timeOfDay.length; i++) {
     let thData = document.createElement('th');
     tableHead.appendChild(thData);
     thData.textContent = timeOfDay[i];
@@ -88,18 +76,6 @@ function renderTableHeader() {
 }
 renderTableHeader();
 
-// function renderTableFooter() {
-//   for (let i = 0; i < Store.totals.length; i++) {
-//     let tfData = document.createElement('th');
-//     tableFooter.appendChild(tfData);
-//     tfData.textContent = Store.totals[i];
-//   }
-// }
-// renderTableFooter();
-
-
-
-// Section1: Stores data from ALL stores
 
 let seattle = new Store('Seattle', 23, 65, 6.3, [], 0);
 let tokyo = new Store('Tokyo', 3, 24, 1.2, [], 0);
@@ -107,33 +83,3 @@ let dubai = new Store('Dubai', 11, 38, 3.7, [], 0);
 let paris = new Store('Paris', 20, 38, 2.3, [], 0);
 let lima = new Store('Lima', 2, 16, 4.6, [], 0);
 
-function renderStoreName() {
-
-console.log(storeName);
-}
-
-console.log();
-
-this.renderTableFooter(seattle);
-
-// console.log(tokyo);
-// console.log(dubai);
-// console.log(paris);
-// console.log(lima);
-// all tests pass
-// end of Section1
-
-// this.render function () {
-//   this.getTime();
-//   let list = document.createElement('ul');
-//   mypage.appendChild(list);
-//   list.textContent = `${this.name}`;
-//   for (let i = 0; i < this.sales.length; i++) {
-//     let listAmt = document.createElement('li');
-//     list.appendChild(listAmt);
-//     listAmt.textContent = `${timeOfDay[i]}: ${this.sales[i]}`;
-//   }
-//   let totalAmt = document.createElement('li');
-//   list.appendChild(totalAmt);
-//   totalAmt.textContent = `Total: ${this.totalSale}`;
-// }
